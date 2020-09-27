6175dcfd1e6339c72e283c1c1bed95f0c4195ecc
nikita_bais <nikita.bais2@gmail.com>
2020-09-27 18:35:18 +0530

    Add project files.

---
+++ CreateAPI/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CreateAPI.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Department { get; set; }
+        public double Salary { get; set; }
+
+    }
+}
